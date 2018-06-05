e4f9c4d74ee9fc93c1c8bfddef8cc83d7d76659f
Neel Akash <theofficialneel@gmail.com>
2018-06-05 16:25:32 +0530

    scene2 widget elements

--- src/Custom/Widgets/Scene2NodeWidget.js
+++ src/Custom/Widgets/Scene2NodeWidget.js
@@ -11,13 +11,47 @@ export class Scene2NodeWidget extends React.Component {
     constructor(props) {
         super(props);
         console.log(props);
+        this.state = {
+            hsArray: []
+        };
     }
+
+    _addElement = () => {
+        let hsLength = this.state.hsArray.length;
+        let nextNum =  (hsLength>0)?this.state.hsArray[hsLength-1].hsNum+1:1;
+        let newHsArray = Object.assign([],this.state.hsArray);
+        newHsArray.push({hsNum: nextNum});
+        this.setState({
+            hsArray: newHsArray
+        });
+        this.props.node.addNextPort("N");
+    };
+    _removeElement = () => {
+        let newHsArray = Object.assign([],this.state.hsArray);
+        newHsArray.pop();
+        this.setState({
+            hsArray: newHsArray
+        });
+        if(this.props.node.getNextPorts().length !== 0)
+            this.props.node.removePort(this.props.node.getNextPorts().pop());
+    };
+    _generateElement = () => {
+        return this.state.hsArray.map((hotspot,index)=> (
+            <div key={index}>HS {hotspot.hsNum} {this.generatePort(this.props.node.getNextPorts()[index])}</div>
+        ));
+    };
 
     generatePort(port) {
         return <SRD.DefaultPortLabel model={port} key={port.id}/>;
     }
 
     render() {
-        return (<div>dsmfk</div>);
+        return (
+            <div>
+                <div>Scene</div>
+                <div onClick={this._addElement}>+</div>
+                <div onClick={this._removeElement}>-</div>
+                <div>{this._generateElement()}</div>
+            </div>);
     }
 }
