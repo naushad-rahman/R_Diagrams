d70988247c2a986c45fa75a71e8ddd094d86a095
Neel Akash <theofficialneel@gmail.com>
2018-06-05 20:57:06 +0530

    scene2 port labels fixed

--- src/Custom/Widgets/BodyWidget.js
+++ src/Custom/Widgets/BodyWidget.js
@@ -107,8 +107,8 @@ export class BodyWidget extends React.Component {
                                     break;
                                 case "Scene2":
                                     node = new Scene2NodeModel();
-                                    node.addOutPort("O");
-                                    node.addInPort("I");
+                                    node.addOutPort(" ");
+                                    node.addInPort(" ");
                                     break;
                                 default :
                                     console.log("Error in Switch");
